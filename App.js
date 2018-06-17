e591fa6b9d46bdfcd68559dcea51141db0554d7b
David Solà <d.sola.03@gmail.com>
2018-06-17 20:59:47 +0200

    Integrate the login form into the application

--- App.js
+++ App.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+
+import Router from './src/Router';
+import reducers from './src/reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 export default class App extends React.Component {
+
+  componentWillMount() {
+    // Initialize Firebase
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Welcome to stamps Card!</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <Router />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 30
-    }
-});
