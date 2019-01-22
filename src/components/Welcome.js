b1902a12c9a96a8b514980cb33874dae7f2d4f80
David Solà <d.sola.03@gmail.com>
2019-01-23 00:45:37 +0100

    Redesign the Welcome screen

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Container } from 'native-base';
-import { Text } from 'react-native';
+import { Text, Image } from 'react-native';
 import { connect } from 'react-redux';
 
 import { SimpleHeader } from './common';
 import { goToMainPage } from '../actions';
+import { CUSTOMER, BUSINESS_OWNER } from '../values/Profiles';
 
 class Welcome extends React.Component {
   componentDidMount() {
@@ -13,16 +14,27 @@ class Welcome extends React.Component {
     }, 1500);
   }
 
+  getSourceImage() {
+    if (this.props.profile.id === CUSTOMER) {
+      return require('../../resources/img/customer-welcome-illustration.png');
+    }
+    if (this.props.profile.id === BUSINESS_OWNER) {
+      return require('../../resources/img/business-owner-welcome-illustration.png');
+    }
+  }
+
   render() {
-    const { containerStyle, textStyle, headerStyle } = styles;
+    const { containerStyle, textStyle, headerStyle, imageStyle } = styles;
+    const sourceImage = this.getSourceImage();
+
     return (
         <Container style={containerStyle}>
           <SimpleHeader style={headerStyle} />
-          <Text style={textStyle}>{'welcome'.toUpperCase()}</Text>
-          <Text style={textStyle}>{'to your'.toUpperCase()}</Text>
-          <Text style={textStyle}>{'stamps card'.toUpperCase()}</Text>
-          <Text style={textStyle}>{'for'.toUpperCase()}</Text>
-          <Text style={textStyle}>{this.props.profile.text.toUpperCase()}</Text>
+          <Image
+            style={imageStyle}
+            source={sourceImage}
+          />
+          <Text style={textStyle}>Welcome, {this.props.user.firstName} {this.props.user.lastName}</Text>
         </Container>
     );
   }
@@ -40,8 +52,13 @@ const styles = {
   },
   textStyle: {
     color: '#fff',
-    fontSize: 45,
+    fontSize: 25,
     fontWeight: 'bold'
+  },
+  imageStyle: {
+    height: 200,
+    width: 200,
+    resizeMode: 'cover'
   }
 };
 
